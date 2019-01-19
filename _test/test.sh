8382c3de83bd0fde234ae24e2195a74fcaf704e2
Yllab <bally.hugo@gmail.com>
2019-01-19 18:23:40 +0100

    getting close to final passes

--- _test/test.sh
+++
@@ -1,3 +0,0 @@
-../ft_ls -Rla $1 > my_ls
-ls -Rla $1 > real_ls
-diff my_ls real_ls > diff_result
--- srcs/sort_dir.c
+++ srcs/sort_dir.c
@@ -24,13 +24,18 @@ static int8_t		cmp_mtime(char *path1, char *path2, int8_t rev)
 {
 	struct stat		stats;
 	int8_t			result;
-	time_t			path1_time;
+	struct timespec	t1;
+	struct timespec	t2;
 
 	ft_bzero(&stats, sizeof(struct stat));
 	get_stats(path1, &stats, NOFOLLOW);
-	path1_time = stats.st_mtimespec.tv_sec;
+	t1 = stats.st_mtimespec;
 	get_stats(path2, &stats, NOFOLLOW);
-	result = path1_time < stats.st_mtimespec.tv_sec;
+	t2 = stats.st_mtimespec;
+	if (t1.tv_sec != t2.tv_sec)
+		result = t1.tv_sec < t2.tv_sec;
+	else
+		result = t1.tv_nsec < t2.tv_nsec;
 	return (rev ? !result : result);
 }
 
