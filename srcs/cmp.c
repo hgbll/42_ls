c3bb712fb48e589bf53748724d310de35c89fb9c
Yllab <bally.hugo@gmail.com>
2019-01-21 01:41:47 +0100

    sort is working

--- srcs/cmp.c
+++ srcs/cmp.c
@@ -13,18 +13,14 @@
 #include "ls.h"
 #include "libft.h"
 
-int8_t				cmp_ascii(char *name1, char *name2, int8_t rev)
+int8_t				cmp_ascii(char *path1, char *path2)
 {
-	int8_t			result;
-
-	result = ft_strcmp(name1, name2) > 0;
-	return (rev ? !result : result);
+	return (ft_strcmp(path1, path2) > 0);
 }
 
-int8_t				cmp_mtime(char *path1, char *path2, int8_t rev)
+int8_t				cmp_mtime(char *path1, char *path2)
 {
 	struct stat		stats;
-	int8_t			result;
 	struct timespec	t1;
 	struct timespec	t2;
 
@@ -34,14 +30,8 @@ int8_t				cmp_mtime(char *path1, char *path2, int8_t rev)
 	get_stats(path2, &stats, NOFOLLOW);
 	t2 = stats.st_mtimespec;
 	if (t1.tv_sec != t2.tv_sec)
-	{
-		result = t1.tv_sec < t2.tv_sec;
-		return (rev ? !result : result);
-	}
+		return (t1.tv_sec < t2.tv_sec);
 	if (t1.tv_nsec != t2.tv_nsec)
-	{
-		result = t1.tv_nsec < t2.tv_nsec;
-		return (rev ? !result : result);
-	}
-	return (cmp_ascii(path1, path2, 0));
+		return (t1.tv_nsec < t2.tv_nsec);
+	return (cmp_ascii(path1, path2));
 }
--- srcs/sort_dir.c
+++ srcs/sort_dir.c
@@ -21,25 +21,39 @@ static void			entry_swap(t_entry *data, int i1, int i2)
 	data[i2] = swap;
 }
 
-static int			partition(t_entry *data,
-								t_pos pos,
-								cmp_ptr cmp,
-								int8_t rev)
+static void			reverse(t_entry *data, size_t len)
+{
+	size_t			i;
+	size_t			j;
+	size_t			mid;
+
+	i = 0;
+	j = len - 1;
+	mid = len / 2;
+	while (i < mid)
+	{
+		entry_swap(data, i, j);
+		i++;
+		j--;
+	}
+}
+
+static int			partition(t_entry *data, int lo, int hi, cmp_ptr cmp)
 {
 	t_entry			pivot;
 	int				i;
 	int				j;
 
-	pivot = data[(pos.lo + pos.hi) / 2];
-	i = pos.lo - 1;
-	j = pos.hi + 1;
+	pivot = data[(lo + hi) / 2];
+	i = lo - 1;
+	j = hi + 1;
 	while (1)
 	{
 		i++;
-		while (cmp(pivot.path, data[i].path, rev))
+		while (cmp(pivot.path, data[i].path))
 			i++;
 		j--;
-		while (cmp(data[j].path, pivot.path, rev))
+		while (cmp(data[j].path, pivot.path))
 			j--;
 		if (i >= j)
 			return (j);
@@ -47,30 +61,21 @@ static int			partition(t_entry *data,
 	}
 }
 
-static void			quicksort(t_entry *data,
-								t_pos pos,
-								cmp_ptr cmp,
-								int8_t rev)
+static void			quicksort(t_entry *data, int lo, int hi, cmp_ptr cmp)
 {
-	t_pos			new;
 	int				p;
 	
-	if (pos.lo < pos.hi)
+	if (lo < hi)
 	{
-		p = partition(data, pos, cmp, rev);
-		new.lo = pos.lo;
-		new.hi = p;
-		quicksort(data, new, cmp, rev);
-		new.lo = p + 1;
-		new.hi = pos.hi;
-		quicksort(data, new, cmp, rev);
+		p = partition(data, lo, hi, cmp);
+		quicksort(data, lo, p, cmp);
+		quicksort(data, p + 1, hi, cmp);
 	}
 }
 
 int8_t				sort_dir(t_dirlist *dir, t_opt *opt)
 {
 	cmp_ptr			cmp;
-	t_pos			pos;
 	
 	if (opt->sort_mt)
 		cmp = &cmp_mtime;
@@ -78,10 +83,9 @@ int8_t				sort_dir(t_dirlist *dir, t_opt *opt)
 		cmp = &cmp_ascii;
 	if (dir->len > 0)
 	{
-		pos.lo = 0;
-		pos.hi = dir->len - 1;
-		pos.max = pos.hi;
-		quicksort(dir->data, pos, cmp, opt->rev);
+		quicksort(dir->data, 0, (int)(dir->len - 1), cmp);
+		if (opt->rev)
+			reverse(dir->data, dir->len);
 	}
 	return (0);
 }
